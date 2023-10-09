042adcfe0d1e0a45e61cadb8995649f3130f0f74
Petruska Tomi <tamas.petruska99@gmail.com>
2023-10-09 22:29:39 +0200

    deploy fix

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -61,7 +61,7 @@ export default function LoginForm() {
           )}
 
           <Link className="text-sm mt-3 text-right" href={"/register"}>
-            Don't have an account? <span className="underline">Register</span>
+            Don&rsquo;t have an account? <span className="underline">Register</span>
           </Link>
         </form>
       </div>
